9eff1179b4aa5d00808386efddb0d700adddf08e
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-07-26 03:54:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pkg install python3 git -y
 pip3 install six ipcalc requests
-git clone https://github.com/leitura/websocket_cloudflare_Scan
+git clone https://github.com/SuspectWorkers/websocket_cloudflare_Scan
 cd websocket_cloudflare_Scan
 
